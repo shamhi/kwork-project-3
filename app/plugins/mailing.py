7588325ea47ec9c28c4384795d7a311601b7bfd0
ShamHi <shomakakdela@gmail.com>
2024-02-12 20:00:09 +0300

    Add mailing method

---
+++ app/plugins/mailing.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+@Client.on_message(filters.me & filters.private & filters.command(['mailing', 'message'], prefixes="/"))
+async def send_mailing(client: Client, message: Message):
+    text = " ".join(message.command[1:])
+
+    if not text:
+        return await message.reply(
+            text='<emoji id=5210952531676504517>❌</emoji><b>Ты не ввел текст после команды!!!</b>',
+            reply_to_message_id=message.id)
+
+    chats = [obj.chat.id async for obj in client.get_dialogs()]
+    count = 0
+
+    for chat_id in chats:
+        try:
+            await client.send_message(chat_id=chat_id, text=text)
+            count += 1
+        except Exception as er:
+            error_chat = await client.get_chat(chat_id=chat_id)
+            print(f"Не удалось отправить сообщение в чат {error_chat.username}\n"
+                  f"Ошибка: {er}")
+
+    await message.reply(
+        text=f"<emoji id=5206607081334906820>✔️</emoji>Сообщение разослано по {count} из {len(chats)} чатам",
+        reply_to_message_id=message.id)
