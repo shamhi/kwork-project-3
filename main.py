1b9a3aa7a92f0ad299d248f3d84e20cb1f516bf3
ShamHi <shomakakdela@gmail.com>
2024-02-14 18:30:36 +0300

    Update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from os.path import exists
 
-from pyrogram import Client, idle
+from pyrogram import Client, compose
 from pyrogram_patch import patch
 import aiosqlite
 
@@ -11,24 +11,32 @@ from app.middlewares import ConnectDB
 from app.db import Database
 
 async def main():
-    app = Client(
-        name=Config.APP_NAME,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
+    app1 = Client(
+        name=Config.APP_NAME1,
+        api_id=Config.API_ID1,
+        api_hash=Config.API_HASH1,
         plugins=dict(root='plugins' if exists('plugins') else 'app/plugins')
     )
 
-    patch_manager = patch(app=app)
-    patch_manager.include_middleware(ConnectDB(db_name="app1_db"))
+    app2 = Client(
+        name=Config.APP_NAME2,
+        api_id=Config.API_ID2,
+        api_hash=Config.API_HASH2,
+        plugins=dict(root='plugins' if exists('plugins') else 'app/plugins')
+    )
 
-    await app.start()
+    patch_manager1 = patch(app=app1)
+    patch_manager2 = patch(app=app2)
 
-    async with aiosqlite.connect('app/db/app1_db.db') as temp_conn:
-        db = Database(conn=temp_conn)
-        await db.create_tables()
+    patch_manager1.include_middleware(ConnectDB(db_name=app1.name))
+    patch_manager2.include_middleware(ConnectDB(db_name=app2.name))
 
-    await idle()
-    await app.stop(block=True)
+    for app in (app1, app2):
+        async with aiosqlite.connect(f'app/db/{app.name}.db') as temp_conn:
+            db = Database(conn=temp_conn)
+            await db.create_tables()
+
+    await compose([app1, app2])
 
 
 if __name__ == '__main__':
